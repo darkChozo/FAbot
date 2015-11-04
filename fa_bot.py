84cbda6e63291938a38b6c24ba9cfb647a5a653a
Mark Dennehy <mark.dennehy@workday.com>
2015-11-04 17:38:00 +0000

    merging before refactor

--- fa_bot.py
+++ fa_bot.py
@@ -1,64 +1,97 @@
 #!/usr/local/bin/python
 import logging
-from config_manager import ConfigManager
-from discord_client import main_client
-from event_manager import event_manager
-import game_servers
-from variables_manager import variables_manager
-
+import re
+import discord_client
+import config_manager
+import event_manager
+import game_server
+import watcher
+import command
 
 class FAbot(object):
+    discordClient = None
+    event_manager = None
+    main_watcher = None
+    game_servers = {}
+    commandregex = re.compile("(?s)^!(?P<command>\w+)\s*(?P<args>.*)?")
+    commands = {}
 
-    @staticmethod
-    def start():
+    @classmethod
+    def command(self,cmd):
+        """
+        When you write a new command, add this decorator to it, so it gets registered
+        """
+        def wrapper(self,cmd):
+            self.commands[cmd] = args[0]
+            return args[0]
+        return wrapper
+
+    def __init__(self):
         # Logging
-        logging.basicConfig(filename="FA_bot.log", level=logging.DEBUG, format="%(asctime)-15s %(message)s")
+        logging.basicConfig(filename="logs/FA_bot.log", level=logging.DEBUG, format="%(asctime)-15s %(message)s")
         logging.info("FAbot starting up")
 
+    def start(self):
         # Configuration file
         logging.info("Reading configuration")
-        config = ConfigManager("config.ini")
+        self.config = config_manager.ConfigManager("config.ini")
 
-        client_email = config.get("email", section="Bot Account")
-        client_pass = config.get("password", section="Bot Account")
-        event_manager.announcement_channels = config.get_json("announcement_channels", default=[])
+        self.event_manager = event_manager.EventManager()
+        self.main_watcher = watcher.Watcher(self)
+
+        self.event_manager.announcement_channels = self.config.get_json("announcement_channels", default=[])
         # TODO: probably event manager should take channels from client instead?
 
-        main_client.channel_whitelist = config.get_json("channel_whitelist", default=[])
-        main_client.announcement_channels = config.get_json("announcement_channels", default=[])
-
-        main_client.welcome_pm = config.get("welcome_pm", section="Announcements")
-        main_client.join_announcement = config.get("join_announcement", section="Announcements")
-        main_client.leave_announcement = config.get("leave_announcement", section="Announcements")
-
         # Game servers
-        game_servers.game_servers['arma'] = game_servers.ArmaServer(
-            ip=config.get("arma_server_ip"),
-            port=int(config.get("arma_server_port"))
+        self.game_servers['arma'] = game_server.ArmaServer(
+            ip=self.config.get("arma_server_ip"),
+            port=int(self.config.get("arma_server_port"))
         )
-        game_servers.game_servers['insurgency'] = game_servers.InsurgencyServer(
-            ip=config.get("insurgency_server_ip"),
-            port=int(config.get("insurgency_server_port"))
+        self.game_servers['insurgency'] = game_server.InsurgencyServer(
+            ip=self.config.get("insurgency_server_ip"),
+            port=int(self.config.get("insurgency_server_port"))
         )
 
         # Discord client
         logging.info("Logging into Discord")
-        main_client.login(client_email, client_pass)
+        self.discordClient = discord_client.Client(self)
 
-        if not main_client.is_logged_in:
+        self.discordClient.channel_whitelist = self.config.get_json("channel_whitelist", default=[])
+        self.discordClient.announcement_channels = self.config.get_json("announcement_channels", default=[])
+
+        self.discordClient.welcome_pm = self.config.get("welcome_pm", section="Announcements")
+        self.discordClient.join_announcement = self.config.get("join_announcement", section="Announcements")
+        self.discordClient.leave_announcement = self.config.get("leave_announcement", section="Announcements")
+
+        self.client_email = self.config.get("email", section="Bot Account")
+        self.client_pass = self.config.get("password", section="Bot Account")
+        if self.client_email == None or self.client_pass == None:
+            logging.critical("Could not find Discord authentication details in config file.")
+            print("Could not find Discord authentication details in config file.")
+            exit(1)
+
+        self.discordClient.login(self.client_email, self.client_pass)
+
+        if not self.discordClient.is_logged_in:
             logging.critical("Logging into Discord failed")
             print('Logging in to Discord failed')
             exit(1)
 
+        logging.info("Starting main watcher")
+        self.main_watcher.start()
+
         logging.info("Entering main message event loop")
-        main_client.run()
+        self.discordClient.run()
+
 
 
 # Full sail ahoy!
 if __name__ == '__main__':
     try:
-        FAbot.start()
+        fa_bot = FAbot()
+
+        fa_bot.start()
     except KeyboardInterrupt:
         print "Disconnecting..."
         logging.info("Keyboard interrupt. Disconnecting.")
-        main_client.logout()
+        fa_bot.discordClient.logout()
